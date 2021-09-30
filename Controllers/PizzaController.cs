8f41b8be802f680c9e8c9bbd3dc4f732eb87f604
Vitor R <vitor_rc@outlook.com>
2021-09-30 19:22:06 -0300

    PizzaController - Get e GetAll

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,44 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll()
+        {
+            return PizzaService.GetAll();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if(pizza == null)
+            {
+                return NotFound();
+            }
+
+            return pizza;
+        }
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
